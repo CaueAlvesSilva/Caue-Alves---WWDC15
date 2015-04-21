1967759ca2ee9a467dc09655f9c5e5747a34542a
CaueAlvesSilva <cauesilva@MacBook-Pro-de-Caue.local>
2015-04-21 16:53:19 -0300

    View Profile criada

---
+++ CaueAlves - WWDC15/CaueAlves - WWDC15/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  CaueAlves - WWDC15
+//
+//  Created by Cauê Silva on 21/04/15.
+//  Copyright (c) 2015 Cauê Silva. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = "Profile"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
