47b8267cdb4726d0de862c4f462b7a7cf630e7a8
CaueAlvesSilva <cauesilva@MacBook-Pro-de-Caue.local>
2015-04-21 21:36:13 -0300

    teste page control

--- CaueAlves - WWDC15/CaueAlves - WWDC15/DetailViewController.swift
+++ CaueAlves - WWDC15/CaueAlves - WWDC15/DetailViewController.swift
@@ -11,15 +11,33 @@ import UIKit
 class DetailViewController: UIViewController {
 
     var type:String!
+    var pages: [(pageNumber: Int, showPhoto: String, showText: String)] = []
+
+    @IBOutlet weak var imgPhoto: UIImageView!
+    @IBOutlet weak var lblText: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.navigationItem.title = type
+        
+        pages = [(1,"photo1.png", "Text of photo 1"),
+                 (2,"photo2.png", "Text of photo 2"),
+                 (3,"photo3.png", "Text of photo 3")]
+        
+//        imgPhoto.image = UIImage(named: pages[1].showPhoto)
+//        lblText.text = pages[1].showText
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    
+    @IBAction func changePage(sender: UIPageControl) {
+        
+        imgPhoto.image = UIImage(named: pages[sender.currentPage].showPhoto)
+        lblText.text = pages[sender.currentPage].showText
     }
     
 
